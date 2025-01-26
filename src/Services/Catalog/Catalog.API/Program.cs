75e0c9892513e6fbbab46617f8d54cc0b12283ff
Timallbr <timallbr@gmail.com>
2025-01-26 15:04:40 -0700

    Adding CreateProduct / GetProducts QueryHandler and Endpoint classes. Updated settings for using PostgreSQL. Updated docker-compose settings

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to container
+
+//builder.Services.AddSingleton<ILoggerFactory, LoggerFactory>();
+//builder.Services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+//builder.Services.AddTransient<ILogger>(p =>
+//{
+//    var loggerFactory = p.GetRequiredService<ILoggerFactory>();
+//    // You could also use the HttpContext to make the name dynamic for example
+//    return loggerFactory.CreateLogger("my logger");
+//});
+
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    //options.AutoCreateSchemaObjects
+}).UseLightweightSessions();
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
