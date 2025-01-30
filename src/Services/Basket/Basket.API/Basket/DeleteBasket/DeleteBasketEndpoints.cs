9e642f99ba4b46e7c365a06d7a322a109751b720
Timallbr <timallbr@gmail.com>
2025-01-30 16:58:48 -0700

    Completed Basket.API project. Updated docker settings Added AspNetCore.HealthChecks Added Scrutor for decorator Added Redis caching

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -0,0 +1,29 @@
+﻿
+using Basket.API.Basket.GetBasket;
+
+namespace Basket.API.Basket.DeleteBasket
+{
+    // public record DeleteBasketRequest(string UserName);
+    public record DeleteBasketResponse(bool IsSuccess);
+
+    public class DeleteBasketEndpoints : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+
+                var reponse = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(reponse);
+            })
+            .WithName("DeleteProduct")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
+    }
+}
